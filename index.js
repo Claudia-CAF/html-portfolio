107a1d03ddec1ac10268dd57e1ca60e3de99f10c
Claudette <100962684+Claudia-CAF@users.noreply.github.com>
2025-07-22 16:30:26 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function test() {
+  var a = "3";
+  var b = "8";
+  
+/* Do not change the code above */
+// Whrite your code on lines 7 - 9
+  
+
+
+
+  /* Do not change the code below */
+  console.log("a is " + a);
+  console.log("b is " + b);
+}
